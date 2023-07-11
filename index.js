eaf9e85d74bd809a27242a3a83a5eb9464b92240
Mercy2525 <mercywmuriithi.mm@gmail.com>
2023-07-11 23:45:37 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+    if (street>42) {
+        return (street-42);
+    } else if (street<42) {
+        return (42- street);
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    let blockCount=distanceFromHqInBlocks(street)
+    return blockCount * 264
+}
+
+function distanceTravelledInFeet(start,stop) {
+
+    if (stop>start) {
+        return (stop-start)*264
+    } else if (stop<start) {
+        return (start-stop) * 264
+    }
+}
+
+function calculatesFarePrice(start,stop){
+  let length =distanceTravelledInFeet(start, stop)
+
+    if (length<=400) {
+        return 0;
+    } else if (length>400 && length<=2000 ) {
+        return ((length-400)* 0.02)
+    } else if (length > 2000 && length<=2500) {
+        return 25;
+    }else if (length> 2500) {
+        return "cannot travel that far"
+    }
+}
